949d4d87db0a58cb6a8654aa2470b6aa9d3d1340
Mauricio211 <71468645+Mauricio211@users.noreply.github.com>
2020-12-02 23:37:12 -0800

    Update hola.s

--- code/hola.s
+++ code/hola.s
@@ -1,3 +1,11 @@
+@ Nombre del Alumno:Mauricio Chavez Arroyo
+@ Numero de control:18212159
+@ Periodo:Septiembre-Enero 2020
+@ Practica:3.6
+@ Materia:Lenguajes de interfaz
+@ Profesor:Rene Solis Reyes
+@ Nombre del programa:hola.s
+
 .global _start
 _start:
   MOV R7, #4
